16f5f0d519b8eeb3ac16eef545965619be12cf28
erose357 <erose357@gmail.com>
2018-03-31 09:09:54 -0600

    wip - building search controller

--- spec/features/user_can_search_for_characters_spec.rb
+++ spec/features/user_can_search_for_characters_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "User can search" do
   scenario "by character name" do
-    #As a user
-    #when i visit the root path
+
     visit '/'
-    #And I fill in the search form with 'deadpool'
-    fill_in 'Name', with: 'wolvervine'
-    #And I click search
+
+    fill_in 'name', with: 'deadpool'
+
     click_on 'Search'
-    #I should be on the page '/search' (maybe: with parameters visible in the url?)
+
     expect(current_path).to eq('/search')
     #I should see the name Wolverine, a description of the character, and a image of the character
     expect(page).to have_content('Wolverine')
