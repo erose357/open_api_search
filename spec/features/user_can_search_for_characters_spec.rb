cafc6e5dfa641a05ce5ce6e27e2eefdc454c7183
erose357 <erose357@gmail.com>
2018-03-31 12:17:39 -0600

    Refactors search_controller

--- spec/features/user_can_search_for_characters_spec.rb
+++ spec/features/user_can_search_for_characters_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "User can search" do
-  scenario "by character name" do
+  scenario "by character full name" do
     VCR.use_cassette('full_name', :match_requests_on => [:host]) do
 
     visit '/'
@@ -13,8 +13,11 @@ RSpec.feature "User can search" do
     expect(current_path).to eq('/search')
 
     expect(page).to have_content('Wolverine')
-    expect(page).to have_content('')
-    expect(page).to have_content('')
+    expect(page).to have_content('Born with super-human senses and the power to heal from almost any wound')
+    expect(page).to have_css("img[src='http://i.annihil.us/u/prod/marvel/i/mg/2/60/537bcaef0f6cf.jpg']")
+    expect(page).to have_content('Wolverine (Ultimate)')
+    expect(page).to have_content('Decades after participating in military airdrops with Captain America during WWII')
+    expect(page).to have_css("img[src='http://i.annihil.us/u/prod/marvel/i/mg/9/03/531773b76840c.jpg']")
     end
   end
 end
