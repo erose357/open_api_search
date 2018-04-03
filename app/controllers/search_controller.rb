0342d99565ce4d13cb20faae8370bf84695be25c
erose357 <erose357@gmail.com>
2018-04-03 14:25:01 -0600

    Fixes search_controller ordering

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,13 @@
 class SearchController < ApplicationController
   def index
     search_name = params[:title] || params[:format]
-    @searches = order_previous_searches(params[:order])
-    @movies = TmdbService.new(search_name).movie_list
-    @search = Search.find_by(search_term: search_name)
+
     if params[:order].nil?
       Search.update_or_create(search_name)
     end
+
+    @searches = order_previous_searches(params[:order])
+    @movies = TmdbService.new(search_name).movie_list
+    @search = @searches.find { |search| search.search_term == search_name }
   end
 end
